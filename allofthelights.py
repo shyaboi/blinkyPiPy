8fb77a847fcb149622cf0361cb856f34902974b4
shyaboi <ian.shane.sears@gmail.com>
2020-09-22 15:42:26 -0700

    random function added for red light fun...

--- allofthelights.py
+++ allofthelights.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
-import sys
-import time
+import sys, time, random
 
 
 GPIO.setmode(GPIO.BCM)
@@ -9,6 +8,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(21,GPIO.OUT)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20,GPIO.OUT)
+
+
 n=1
 
 
@@ -22,24 +23,31 @@ def green():
     GPIO.output(21, GPIO.LOW)
 def red():
     GPIO.output(20,GPIO.HIGH)
-    time.sleep(5)
+    time.sleep(6)
     GPIO.output(20, GPIO.LOW)
 def lightLoop(n):
-    if n%3==0&n%2==0:
+    rando = random.randint(n-333, n+333)
+    if n >= rando:
+        print(rando)
         red()
+        time.sleep(2)
+        lightLoop(n)
     if n%2==0:
+        print(f"blue{n}")
         n+=1
-        print(n)
         blue()
         time.sleep(2)
         lightLoop(n)
     if n%3==0:
         green()
+        print(f"green{n}")
+        n+=1
         time.sleep(2)
         lightLoop(n)
     else:
-        lightLoop(n)
         n+=1
+        lightLoop(n)
+        
 
     if KeyboardInterrupt:
         GPIO.setmode(GPIO.BCM)
