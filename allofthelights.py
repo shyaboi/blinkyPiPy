3e45852602b985a80ebd83b5228efe7ca4b536f8
shyaboi <ian.shane.sears@gmail.com>
2020-09-22 19:55:34 -0700

    endless blink seems to work, and exit divide by 0 worked

--- allofthelights.py
+++ allofthelights.py
@@ -16,65 +16,65 @@ n=1
 
 def blue():
     GPIO.output(18,GPIO.HIGH)
-    time.sleep(.2)
+    time.sleep(.05)
     GPIO.output(18, GPIO.LOW)
 def green():
     GPIO.output(21,GPIO.HIGH)
-    time.sleep(.2)
+    time.sleep(.05)
     GPIO.output(21, GPIO.LOW)
 def red():
     GPIO.output(20,GPIO.HIGH)
-    time.sleep(.2)
+    time.sleep(.05)
     GPIO.output(20, GPIO.LOW)
 def yellow():
     GPIO.output(26,GPIO.HIGH)
-    time.sleep(.2)
+    time.sleep(.05)
     GPIO.output(26, GPIO.LOW)
+    
+    
 def lightLoop(n):
     rando = random.randint(n-333, n+333)
     if rando == 0:
         rando = rando+1
+        #print('i dont play 0')
+        #GPIO.setmode(GPIO.BCM)
+       # GPIO.setup(18,GPIO.OUT)
+       # GPIO.output(18, GPIO.LOW)
+       # GPIO.setup(21,GPIO.OUT)
+       # GPIO.output(21, GPIO.LOW)
+       # GPIO.setup(20,GPIO.OUT)
+      #  GPIO.output(20, GPIO.LOW)
+        print('woops, almost divided by 0')
+        #GPIO.cleanup()
     if n < rando:
         print(f"red{rando}")
         red()
         n*rando
         print(f"red rando muli{rando}")
-        time.sleep(.01)
+        time.sleep(.001)
         pass
     if n >= rando:
         print(f"yeller{rando}")
         yellow()
         n/-rando
         print(f"yellow rando divide{rando}")
-        time.sleep(.01)
+        time.sleep(.001)
         pass
     if n%2==0:
         print(f"blue{n}")
         n+=1
         blue()
-        time.sleep(.01)
+        time.sleep(.001)
         lightLoop(n)
     if n%3==0:
         green()
         print(f"green{n}")
-        n+=1
-        time.sleep(.01)
+        n-=1
+        time.sleep(.001)
         lightLoop(n)
     else:
         n+=1
         lightLoop(n)
-        
-
-    if KeyboardInterrupt:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18,GPIO.OUT)
-        GPIO.output(18, GPIO.LOW)
-        GPIO.setup(21,GPIO.OUT)
-        GPIO.output(21, GPIO.LOW)
-        GPIO.setup(20,GPIO.OUT)
-        GPIO.output(20, GPIO.LOW)
-        print('quitted')
-        GPIO.cleanup()
 
         
 lightLoop(n)
